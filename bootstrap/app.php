d87fd478699c366b5b0c84f7247bdad73d17ad0e
zudhapr <zudhapratama@gmail.com>
2026-04-04 10:47:33 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
